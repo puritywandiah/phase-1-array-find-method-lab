1b23b66c3c42114bb3cc79c27bb8516d7f60d59b
+ <+>
2022-06-10 05:54:01 +0300

    commit tree 1cb1755151c27c784161beab1fc477dda5ba90f5

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+/*
+function suberbowlWin(records){
+   let rec = records. find(element => element.result === 'W')
+   if (rec) { return rec . year}
+}
+*/
+
+function superbowlWin(record){
+    const match = record.find((obj) => obj.result === "W")
+    if (match == undefined) {
+        return undefined
+    }
+    else {
+        return match.year
+    }
+}
